2352071a68388c119453202d3b2532133b7fe89a
ielmie <efort063@uottawa.ca>
2023-03-15 19:41:21 -0400

    Create creating_dimension_demographics.sql
    Added the sql code to create the demographics dimension

---
+++ sql/creating_dimension_demographics.sql
@@ -0,0 +1,15 @@
+CREATE TABLE dim_demographics (
+	demographics_key INTEGER PRIMARY KEY,
+	postal_code VARCHAR(6), -- LNLNLN
+	dissemination_area SMALLINT,
+	population INTEGER,
+	average_age FLOAT CHECK (average_age BETWEEN 0 AND 150),
+	median_age FLOAT CHECK (median_age BETWEEN 0 AND 150),
+	total_dwellings INTEGER CHECK (total_dwellings>=0),
+	average_family_size FLOAT CHECK (average_family_size>=1),
+	average_income NUMERIC(11,2) CHECK (average_income >= 0),
+	median_income NUMERIC(11,2) CHECK (median_income >= 0),
+	mother_tongue_official_percentage FLOAT CHECK (mother_tongue_official_percentage BETWEEN 0 and 100),
+	mother_tongue_unofficial_percentage FLOAT CHECK (mother_tongue_official_percentage BETWEEN 0 and 100),
+	census_year INTEGER CHECK (census_year=2011 OR census_year=2016 OR census_year=2021)
+);
