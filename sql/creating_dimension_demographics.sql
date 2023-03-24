59dd7e1c703d5ad349d008b196d4a212156f4b8e
ielmie <efort063@uottawa.ca>
2023-03-24 19:05:31 -0400

    demographics for debugging
    demographics still bugged

--- sql/creating_dimension_demographics.sql
+++ sql/creating_dimension_demographics.sql
@@ -3,11 +3,10 @@ CREATE TABLE dim_demographics (
 	dissemination_area INTEGER,
 	population INTEGER,
 	median_age FLOAT CHECK (median_age BETWEEN 0 AND 150),
-	total_dwellings INTEGER CHECK (total_dwellings>=0),
-	average_household_size FLOAT CHECK (average_household_size_size>=1),
-	average_income NUMERIC(11,2) CHECK (average_income >= 0),
-	median_income NUMERIC(11,2) CHECK (median_income >= 0),
+	total_dwellings FLOAT CHECK (total_dwellings>=0),
+	average_household_size FLOAT CHECK (average_household_size>=1),
+	median_household_income NUMERIC(11,2) CHECK (median_household_income >= 0),
 	mother_tongue_official_percentage FLOAT CHECK (mother_tongue_official_percentage BETWEEN 0 AND 100),
-	mother_tongue_unofficial_percentage FLOAT CHECK (mother_tongue_official_percentage BETWEEN 0 AND 100),
+	mother_tongue_unofficial_percentage FLOAT CHECK (mother_tongue_unofficial_percentage BETWEEN 0 AND 100),
 	census_year INTEGER CHECK (census_year=2011 OR census_year=2016 OR census_year=2021)
 );
