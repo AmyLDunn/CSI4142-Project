6bf4fee2defa11d057a0ad05e0f66cd1ae8da810
ielmie <efort063@uottawa.ca>
2023-03-24 18:28:05 -0400

    fixing demographics dimension
    changing path to csv files and fixing sql to be identical

--- sql/creating_dimension_demographics.sql
+++ sql/creating_dimension_demographics.sql
@@ -4,7 +4,7 @@ CREATE TABLE dim_demographics (
 	population INTEGER,
 	median_age FLOAT CHECK (median_age BETWEEN 0 AND 150),
 	total_dwellings INTEGER CHECK (total_dwellings>=0),
-	average_family_size FLOAT CHECK (average_family_size>=1),
+	average_household_size FLOAT CHECK (average_household_size_size>=1),
 	average_income NUMERIC(11,2) CHECK (average_income >= 0),
 	median_income NUMERIC(11,2) CHECK (median_income >= 0),
 	mother_tongue_official_percentage FLOAT CHECK (mother_tongue_official_percentage BETWEEN 0 AND 100),
