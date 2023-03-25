732a94ace2476a9f4de72c1c56c267d0b12655aa
Amy <91133083+AmyLDunn@users.noreply.github.com>
2023-03-24 21:39:03 -0400

    Update creating_fact_table.sql

--- sql/creating_fact_table.sql
+++ sql/creating_fact_table.sql
@@ -21,11 +21,11 @@ CREATE TYPE fire_alarm AS ENUM ('no fire alarm',
 
 
 CREATE TABLE fact_fire_incidents (
-	location_key BIGINT FOREIGN KEY dim_location(location_key),
-	date_key BIGINT FOREIGN KEY dim_date(date_key),
-	weather_key BIGINT FOREIGN KEY dim_weather(weather_key),
-	demographics_key BIGINT FOREIGN KEY dim_demographics(demographics_key),
-	fire_ward_key INTEGER FOREIGN KEY dim_fire_ward(fire_ward_key),
+	location_key BIGINT REFERENCES dim_location(location_key),
+	date_key BIGINT REFERENCES dim_date(date_key),
+	weather_key BIGINT REFERENCES dim_weather(weather_key),
+	demographics_key BIGINT REFERENCES dim_demographics(demographics_key),
+	fire_ward_key INTEGER REFERENCES dim_fire_ward(fire_ward_key),
 	response_time SMALLINT CHECK (response_time >= 0),
 	damage_cad NUMERIC(11,2) CHECK (damage_cad >= 0),
 	casualties SMALLINT CHECK (casualties >= 0),
